6de0684aafe8a53f9c9bd09f648d35f594359e7f
Tony <fangtopmaster@gmail.com>
2017-02-08 16:40:13 +0800

    static inner class fix

--- java/inner_class.java
+++ java/inner_class.java
@@ -1,5 +1,6 @@
 public class inner_class{
 	private String name = "Tony";
+	private static String nick = "Tony";
 
 	public class Inner{
 		String name = "Fang";
@@ -8,11 +9,22 @@ public class inner_class{
 			System.out.println("inner name:" + name);
 		}
 	}
+
+	public static class SInner{
+		String nick = "Fang";
+		public void show (){
+			System.out.println("static outer name:" + inner_class.nick);
+			System.out.println("static inner name:" + nick);
+		}
+	}
 	
 	public static void main(String[] args){
 		inner_class inn = new inner_class();
 		Inner in = inn.new Inner();
+
+		SInner si = new SInner();
 		in.show();
+		si.show();
 	}
 }
 
